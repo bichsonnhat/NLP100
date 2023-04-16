e4a29893596d5df428b3f43183ea8a8950bf693c
Bich Nhat <bichsonnhat@gmail.com>
2023-04-17 00:14:31 +0700

    Update Code

--- Chapter 6 - Machine Learning/50. Download and Preprocess DatasetPermalink.py
+++ Chapter 6 - Machine Learning/50. Download and Preprocess DatasetPermalink.py
@@ -20,7 +20,7 @@ print(shuffled_df)
 # Using 'train_test_split()' to split data
 # test_size = 0.8 = 80%
 # stratify: An array or Series containing the target variable 
-train, train_test = train_test_split(df, test_size = 0.8, shuffle = True, random_state = 42,  stratify = df['CATEGORY'])
+train, train_test = train_test_split(df, test_size = 0.2, shuffle = True, random_state = 42,  stratify = df['CATEGORY'])
 # the output of train_test_split() is assigned to a tuple containing two variables: train and valid_test. 
 # The train variable is assigned to the training set DataFrame, and the valid_test variable is assigned to the validation/test set DataFrame.
 # train is a DataFrame, and valid_test is a DataFrame.
@@ -28,10 +28,21 @@ train, train_test = train_test_split(df, test_size = 0.8, shuffle = True, random
 # Note Code: train = train_test_split(df, test_size = 0.8, shuffle = True, random_state = 42,  stratify=df['CATEGORY'])
 # train is a tuple containing two DataFrames: the training set and the validation/test set.
 # type(train) is list 
-valid, valid_test = train_test_split(df, test_size = 0.1, shuffle = True, random_state = 42,  stratify = df['CATEGORY'])
-test, test_test = train_test_split(df, test_size = 0.1, shuffle = True, random_state = 42,  stratify = df['CATEGORY'])
+valid, valid_test = train_test_split(df, test_size = 0.9, shuffle = True, random_state = 42,  stratify = df['CATEGORY'])
+test, test_test = train_test_split(df, test_size = 0.9, shuffle = True, random_state = 42,  stratify = df['CATEGORY'])
 
 # save data
 train.to_csv('./train.txt', sep='\t', index=False)
 valid.to_csv('./valid.txt', sep='\t', index=False)
 test.to_csv('./test.txt', sep='\t', index=False)
+
+# count
+# shape return the number of rows and columns in the DataFrame or array, respectively.
+print('train', train.shape)
+print(train['CATEGORY'].value_counts())
+print('\n')
+print('valid', valid.shape)
+print(valid['CATEGORY'].value_counts())
+print('\n')
+print('test', test.shape)
+print(test['CATEGORY'].value_counts())
